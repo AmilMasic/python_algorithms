8f5fc364d152bfd3990c16e5b4c34efbf26757d3
Amil Masic <amil.masic@gmail.com>
2022-01-20 15:51:00 -0500

    comments about the video solution

--- return_duplicate_letter.py
+++ return_duplicate_letter.py
@@ -1,7 +1,8 @@
 # Given a string, create a function that returns the first repeating character
 # if such doesn't exist, return the null character
 
-# my solution 
+# my solution
+# using a hashmap
 def first_repeating_character(words):
     check_letters = {}
     for letter in words:
@@ -10,6 +11,19 @@ def first_repeating_character(words):
         else:
             check_letters[letter] = True
     return '\0'
+
+
+
+# ***Video solutions ***
+# Brute force is having two for loops
+
+# sorting doesn't work since we need the first repeating character
+# sorting could give back the char that was not the first one being repeated
+
+# Hash table solution
+# Exactly how I did it
+# it's O(n) time and space complexity
+# def first_repeating_character(words):
 
 
 print(first_repeating_character("asdsagvbb"))
